a41396c6f5215cf3592654821bba047625f301e0
mdsanima <marcinrozewski@gmail.com>
2020-10-11 21:22:36 +0200

    remove tabs sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,11 +25,18 @@ release = '0.1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# extensions = [
+#     'sphinx.ext.autodoc', 
+#     'sphinx.ext.napoleon', 
+#     'recommonmark',
+#     'sphinx_tabs.tabs',
+#     'sphinx-prompt',
+# ]
+
 extensions = [
     'sphinx.ext.autodoc', 
     'sphinx.ext.napoleon', 
     'recommonmark',
-    'sphinx_tabs.tabs',
     'sphinx-prompt',
 ]
 
