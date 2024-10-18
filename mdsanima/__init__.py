17d62993a729afdb5e0ccc7da07a130eb86b0a74
Marcin Różewski <marcinrozewski@gmail.com>
2024-10-18 06:45:47 +0200

    refactor: Update package formatting (#9)

--- mdsanima/__init__.py
+++ mdsanima/__init__.py
@@ -1,3 +1,12 @@
-from .animation import count_down
-from .animation import count_up
-from .render import render_time
+# Copyright (c) 2020 - 2024 Marcin Różewski, MDSANIMA LAB. All rights reserved.
+# Licensed under the MIT license.
+
+"""The package contains modules that will help in calculating rendering time."""
+
+
+from mdsanima.animation import count_down
+from mdsanima.animation import count_up
+from mdsanima.render import render_time
+
+
+__all__ = ["count_down", "count_up", "render_time"]
